150c073f3915979519fe5cc5f1371ffec1dc1d9c
anton46 <anton.work19@gmail.com>
2019-08-06 15:52:55 +0700

    Inrease coverage

--- app/src/test/java/com/movie/app/net/retrofit/MovieApiBaseUrlTest.kt
+++ app/src/test/java/com/movie/app/net/retrofit/MovieApiBaseUrlTest.kt
@@ -7,6 +7,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -15,7 +16,7 @@ class MovieApiBaseUrlTest {
     @Mock
     lateinit var networkSettingsProvider: NetworkSettingsProvider
 
-    lateinit var apiUrl: MovieApiBaseUrl
+    lateinit var apiUrl: AbstractBaseUrl
 
     @Before
     fun test() {
@@ -29,12 +30,27 @@ class MovieApiBaseUrlTest {
 
     @Test
     fun testGetHostMockEnvironment() {
+        `when`(networkSettingsProvider.getServerEnvironment()).thenReturn(ServerEnvironment.MOCK)
+
+        apiUrl = FakeMovieBaseUrl(networkSettingsProvider)
         apiUrl.getHostForEnvironment(ServerEnvironment.MOCK) shouldEqual "localhost"
+        apiUrl.url().port() shouldEqual 8080
+        apiUrl.url().encodedPath() shouldEqual "/pathEnvironment/"
     }
 
-    @Test
-    fun testGetPathForEnvironment() {
-        apiUrl.getPathForEnvironment() shouldEqual null
-    }
+    class FakeMovieBaseUrl(networkSettingsProvider: NetworkSettingsProvider) :
+        AbstractBaseUrl(networkSettingsProvider) {
 
+        override fun getHostForEnvironment(environment: ServerEnvironment?): String = when (environment) {
+            ServerEnvironment.LIVE -> HOST
+            else -> MOCK
+        }
+
+        override fun getPathForEnvironment(): String = "/pathEnvironment/"
+
+        companion object {
+            const val HOST = "api.themoviedb.org"
+            const val MOCK = "localhost"
+        }
+    }
 }
